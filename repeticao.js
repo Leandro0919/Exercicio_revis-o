f7873ed07ec8b21d48cff5d1a2e65b4219c6da41
Leandro0919 <55665437+Leandro0919@users.noreply.github.com>
2020-04-16 19:21:22 -0300

    6

--- repeticao.js
+++ repeticao.js
@@ -1,7 +1,9 @@
-function repeticao(){
-    var x = 0;
-    while(x < 10){
-        document.write("abc"+"</br>");
-        x++;    
-    }    
-}
+
+    function repeticao(){
+        var x = 0;
+        while(x < 10){
+            document.write("abc"+"</br>");
+            x++;    
+        }
+    }
+click.addEventListener
